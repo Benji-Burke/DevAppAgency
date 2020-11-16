35b370ff230b6899f78a4aab372e4e6ff149caa9
Ben <benjaminstevenburke@gmail.com>
2020-11-16 05:11:21 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import logo from './Infinity-Arrow.svg';
+import Header from './components/Header.jsx';
+import Service from './components/Service.jsx';
+import About from './components/About.jsx';
+import Contact from './components/Contact.jsx';
+import Footer from './components/Footer.jsx';
+
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
+           <Header className="navbar"/>
       <header className="App-header">
+   
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="Headliner">Hello, we are DevApp Agency, a digital agency for your technological needs and dreams.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Contact Us
         </a>
       </header>
+      <About/>
+      <Service/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
